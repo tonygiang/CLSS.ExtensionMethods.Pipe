174e861daa4e0fff7ed290dfa12d3f9c57e77cdc
tonygiang <tony_s_giang@yahoo.com>
2022-07-28 14:35:13 +0700

    Project push.

---
+++ Pipe.cs
@@ -0,0 +1,51 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+
+namespace CLSS
+{
+  public static partial class PipeExtension
+  {
+    /// <summary>
+    /// Takes an object, passes it as an argument to the specified action.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="arg"/>.</typeparam>
+    /// <param name="arg">The object to be used as the argument to
+    /// <paramref name="action"/>.</param>
+    /// <param name="action">The action to be executed with
+    /// <paramref name="arg"/> as the argument.</param>
+    /// <returns>The source object.</returns>
+    public static T Pipe<T>(this T arg, Action<T> action)
+    { action(arg); return arg; }
+
+    /// <summary>
+    /// Takes an object, passes it as an argument to the specified function,
+    /// returns the result.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="arg"/>.</typeparam>
+    /// <typeparam name="TResult">The return type of <paramref name="func"/>.
+    /// </typeparam>
+    /// <param name="arg">The object to be used as the argument to
+    /// <paramref name="func"/>.</param>
+    /// <param name="func">The function to be executed with
+    /// <paramref name="arg"/> as the argument.</param>
+    /// <returns>The result of calling the specified function.</returns>
+    public static TResult Pipe<T, TResult>(this T arg, Func<T, TResult> func)
+    { return func(arg); }
+
+    /// <summary>
+    /// Takes an object, passes it as an argument to the specified action,
+    /// returns the source object.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="arg"/>.</typeparam>
+    /// <typeparam name="TResult">The return type of <paramref name="func"/>.
+    /// </typeparam>
+    /// <param name="arg">The object to be used as the argument to
+    /// <paramref name="func"/>.</param>
+    /// <param name="func">The function to be executed with
+    /// <paramref name="arg"/> as the argument.</param>
+    /// <returns>The result of calling the specified function.</returns>
+    public static T PipeKeep<T, TResult>(this T arg, Func<T, TResult> func)
+    { func(arg); return arg; }
+  }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+﻿# CLSS.ExtensionMethods.Pipe
+
+### Problem
+
+It's common to feed the result of a method as an argument into another method, but when doing so, the reading flow of your code is reversed into right-to-left:
+
+```
+var result = Method4(Method3(Method2(Method1(100))));
+```
+
+It also makes logical errors from one missing or one extra parenthesis easy to miss. Typical C# programmers are not used to parse parenthesis pairs as Lisp programmers are.
+
+You can get around the code comprehension issue by writing more lines of code to assign each method's result to a variable. This creates some mental hurdles since you have to think of logical names to the variables.
+
+```
+var result1 = Method1(100);
+var result2 = Method2(result1);
+var result3 = Method3(result2);
+var result4 = Method4(result3);
+```
+
+F# has a native operator for this use case - the forward pipe operator:
+
+```
+let result = 100 |> Method1 |> Method2 |> Method3 |> Method4;
+```
+
+Unfortunately, no such operator natively exists within the C# language.
+
+### Solution
+
+`Pipe` is an extension method that replicates the syntax of F#'s forward pipe operator. The above first example can be rewritten as follows:
+
+```
+using CLSS;
+
+var result = Method1(100).Pipe(Method2).Pipe(Method3).Pipe(Method4);
+```
+
+In one line, the order of execution still goes from left to right with the `Pipe` syntax.
+
+`Pipe` also accepts lambda expressions:
+
+```
+using CLSS;
+
+FetchSomething()?.Pipe(result => { /* Only execute if result is not null */ };
+```
+
+If passed in a `void` method, `Pipe` returns the caller object.
+
+This package also provides the `PipeKeep` extension method if you want to return the caller object even when passing in a method with non-`void` return type.
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
